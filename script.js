c81cede24e206862a5d59fa17eddf6a9120274d9
metamusk <balberanjarenkim@gmail.com>
2023-02-02 10:18:57 +0800

    Add hover change effects

--- script.js
+++ script.js
@@ -5,14 +5,19 @@ const shareBtnPopup = document.querySelector(".main__share-btn--pop-up");
 const popUp = document.querySelector(".main__pop-up");
 const profileBox = document.querySelector(".main__profile-box");
 const mainText = document.querySelector(".main__text");
+const shareBtnIcon = document.querySelector(".main__share-icon");
 
 shareBtn.addEventListener("click", function (e) {
   popUp.classList.toggle("unhide--pop-up");
 
+  shareBtn.classList.toggle("shareBtnBackground");
+
+  shareBtnIcon.classList.toggle("shareBtnFill");
+
   if (window.screen.width <= 400) {
     profileBox.classList.add("hide");
     shareBtnPopup.classList.add("unhide");
-    mainText.style.marginBottom = "3rem";
+    mainText.classList.add("marginBottom");
   }
 });
 
@@ -20,6 +25,12 @@ shareBtnPopup.addEventListener("click", function () {
   popUp.classList.remove("unhide--pop-up");
 
   shareBtnPopup.classList.remove("unhide");
+
+  mainText.classList.remove("marginBottom");
+
+  shareBtn.classList.remove("shareBtnBackground");
+
+  shareBtnIcon.classList.remove("shareBtnFill");
 
   if (window.screen.width <= 400) {
     profileBox.classList.toggle("hide");
@@ -30,7 +41,7 @@ const resetOnResize = function () {
   popUp.classList.remove("unhide--pop-up");
   profileBox.classList.remove("hide");
   shareBtnPopup.classList.remove("unhide");
-  mainText.style.marginBottom = "0";
+  mainText.classList.remove("marginBottom");
 };
 
 window.onresize = resetOnResize;
